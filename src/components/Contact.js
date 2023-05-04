a127ef34f0836390a87f847b4065d4b791ac2714
Soojin Kim <soojinkk98@gmail.com>
2023-05-04 21:46:33 +0900

    Contact page layout complete and fontawesome imported

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,32 @@
 import React, {useState, useEffect} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import styles from "../styles/contact.module.scss";
 
-const Contact = (props) => {
+const Contact = () => {
 
   return (
-    <div id="contact">
-
+    <div id="contact" className={styles.contactDiv}>
+      <h2>Contact Me</h2>
+      <a href="mailto:soojinkk98@gmail.com" className={styles.contactLink}>
+        <FontAwesomeIcon icon={icon({name:"envelope"})} />
+        <p>soojinkk98@gmail.com</p>
+      </a>
+      <a href="https://goo.gl/maps/qLnHmH28aELjkCsf9" className={styles.contactLink}>
+        <FontAwesomeIcon icon={icon({name:"house"})} />
+        <p>Mississauga, ON</p>
+      </a>
+      <a href="https://www.linkedin.com/soojin-kim1" className={styles.contactLink}>
+        <FontAwesomeIcon icon={icon({name:"linkedin", style:'brands'})} />
+        <p>LinkedIn</p>
+        <FontAwesomeIcon icon={icon({name:"arrow-up-right-from-square"})} />
+      </a>
+      <a href="https://github.com/skim2264" className={styles.contactLink}>
+        <FontAwesomeIcon icon={icon({name:"github", style:'brands'})} />
+        <p>Github</p>
+        <FontAwesomeIcon icon={icon({name:"arrow-up-right-from-square"})} />
+      </a>
+      
     </div>
   )
 };

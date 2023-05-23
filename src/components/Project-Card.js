3c336512c7307083b783354add3b2b76e15d7651
Soojin Kim <soojinkk98@gmail.com>
2023-05-23 15:56:26 +0900

    Project popup deleted

--- src/components/Project-Card.js
+++ src/components/Project-Card.js
@@ -1,30 +1,20 @@
 import React, {useState} from "react";
-import ProjectPopup from "./Project-popup";
 import styles from "../styles/projects.module.scss";
 
 const ProjectCard = (props) => {
   const {project} = props;
 
   const [hovered, setHovered] = useState(false);
-  const [clicked, setClicked] = useState(false);
 
   const toggleTitle = (e) => {
     setHovered(!hovered);
   };
 
-  const displayProject = () => {
-    setClicked(true);
-  };
-
   return (
     <div className={styles.projectCardDiv}>
-      <img className={hovered ? `${styles.greyout} ${styles.projectImg}` : styles.projectImg} src={project.img} alt={project.title} onMouseEnter={toggleTitle} onMouseLeave={toggleTitle} onClick={displayProject}></img>
+      <img className={hovered ? `${styles.greyout} ${styles.projectImg}` : styles.projectImg} src={project.img} alt={project.title} onMouseEnter={toggleTitle} onMouseLeave={toggleTitle}></img>
       {hovered
         ? <h3 className={styles.projectTitle}>{project.title}</h3>
-        : null
-      }
-      {clicked
-        ? <ProjectPopup project={project} setClicked={setClicked}></ProjectPopup>
         : null
       }
     </div>

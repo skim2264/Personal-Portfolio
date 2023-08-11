40166b0d9dc54332f8961ea2854593951a00c29f
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-08-10 23:35:44 -0400

    mobile view configured for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/navbar.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
 
   return (
     <nav id="navbar" className={styles.navbarDiv}>
       <div className={styles.navbarLeft}>
         <a href="/Personal-Portfolio" className={styles.logo}>SOOJIN KIM</a>
       </div>
-      <div className={styles.navbarRight}>
-        <a href="#about" className={styles.navLink}>&lt;About/&gt;</a>
-        <a href="#skills" className={styles.navLink}>&lt;Skills/&gt;</a>
-        <a href="#projects" className={styles.navLink}>&lt;Projects/&gt;</a>
-        <a href="#contact" className={styles.navLink}>&lt;Contact/&gt;</a>
+      <FontAwesomeIcon icon={icon({name:'bars'})} size="5x" className={styles.menuIcon} onClick={() => {setIsNavExpanded(!isNavExpanded);}}></FontAwesomeIcon>
+      <div className={isNavExpanded ? `${styles.navbarRight} ${styles.expanded}` : `${styles.navbarRight}`}>
+        <ul>
+          <li><a href="#about" className={styles.navLink} onClick={() => {setIsNavExpanded(false);}}>&lt;About/&gt;</a></li>
+          <li><a href="#skills" className={styles.navLink} onClick={() => {setIsNavExpanded(false);}}>&lt;Skills/&gt;</a></li>
+          <li><a href="#projects" className={styles.navLink} onClick={() => {setIsNavExpanded(false);}}>&lt;Projects/&gt;</a></li>
+          <li><a href="#contact" className={styles.navLink} onClick={() => {setIsNavExpanded(false);}}>&lt;Contact/&gt;</a></li>
+        </ul>
       </div>
     </nav>
   )
